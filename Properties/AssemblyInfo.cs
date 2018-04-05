d935f7e96f43c1e39123806b0ad056acab85969a
Jeremy Geels <a@jgw.tf>
2018-04-05 05:51:46 +0200

    Edited assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SMaL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LyNN")]
+[assembly: AssemblyDescription("Lazy Neural Network")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SMaL")]
+[assembly: AssemblyCompany("JeremyGG")]
+[assembly: AssemblyProduct("LyNN")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
